d394e71febbcdcfb6afa5f61db1abc9f0d94228a
Ben Kim <ben.kim92@hotmail.com>
2019-06-03 15:17:59 +0900

    chatbot folder

---
+++ Python/chatbot/flaskapp.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+from telegram import say_hello, repeat, conversion, naver, bitcoin
+import pprint
+from decouple import config
+
+
+token = config('TOKEN')
+app = Flask(__name__)
+
+
+@app.route(f"/{token}", methods=["POST"])
+def incoming():
+    # pprint prints dictionary properly formatted for viewing
+    # pprint.pprint(request.get_json())
+
+    message = request.get_json().get('message')
+    if message is not None:
+        sender_id = message.get('from').get('id')
+        sender_name = message.get('from').get('first_name') + " " + \
+            message.get('from').get('last_name')
+        sender_msg = message.get('text')
+        print(sender_id, sender_name, sender_msg)
+
+        # MESSAGE REPLY METHODS HERE
+        if sender_msg == 'hello':
+            say_hello(sender_id, sender_name)
+        elif sender_msg == '환율':
+            conversion(sender_id)
+        elif sender_msg == '네이버':
+            naver(sender_id)
+        elif sender_msg == 'bitcoin':
+            bitcoin(sender_id)
+        else:
+            repeat(sender_id, sender_msg)
+
+    return '', 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Python/chatbot/telegram.py
@@ -0,0 +1,86 @@
+import requests
+from decouple import config
+from bs4 import BeautifulSoup as BS
+import operator
+import collections
+
+token = config('TOKEN')
+
+
+def say_hello(sender_id, sender_name):
+    text = f"Hello, {sender_name}. \nType HELP to get a list of all available functions!"
+    chat_id = sender_id
+    send_url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}"
+    response = requests.get(send_url).json()
+    print(response)
+
+
+def repeat(sender_id, sender_msg):
+    method = "sendMessage"
+    chat_id = sender_id
+    text = sender_msg
+    api_url = f"https://api.telegram.org/bot{token}/{method}?chat_id={chat_id}&text={text}"
+    response = requests.get(api_url)
+    print(response)
+
+
+def conversion(sender_id):
+    url = 'https://finance.naver.com/marketindex/?tabSel=exchange#tab_section'
+    html = requests.get(url).text
+    soup = BS(html, 'html.parser')
+    select = soup.select_one('#exchangeList > li.on > a.head.usd > div > span.value')
+
+    # send result as telegram message
+    method = "sendMessage"
+    chat_id = sender_id
+    text = '지금 원/달러 환율은 ' + select.text
+    api_url = f"https://api.telegram.org/bot{token}/{method}?chat_id={chat_id}&text={text}"
+    response = requests.get(api_url)
+    print(response)
+
+
+def naver(sender_id):
+    url = 'https://www.naver.com/'
+    response = requests.get(url).text
+    soup = BS(response, 'html.parser')
+    results = soup.select('.ah_roll_area.PM_CL_realtimeKeyword_rolling .ah_k')
+
+    idx = 0
+    text = ''
+    for result in results:
+        idx = idx + 1
+        text = text + f"{idx}. {result.text}\n"
+
+    # send result as telegram message
+    method = "sendMessage"
+    chat_id = sender_id
+    api_url = f"https://api.telegram.org/bot{token}/{method}?chat_id={chat_id}&text={text}"
+    response = requests.get(api_url)
+    print(response)
+
+
+def bitcoin(sender_id):
+    url = 'https://www.bithumb.com/'
+    html = requests.get(url).text
+    soup = BS(html, 'html.parser')
+    coins = soup.select('.coin_list tr')
+
+    results = {}
+    for coin in coins:
+        name = coin.select_one('td:nth-of-type(1) p a strong').text.strip()
+        price = float(coin.select_one('td:nth-of-type(2) strong').text.replace(',', '').replace('원', ''))
+        price = round(price)
+        results[name] = price
+
+    sorted_results = sorted(results.items(), key=operator.itemgetter(1), reverse=True)
+    text = ''
+    for result in sorted_results:
+        name, price = result
+        text = text + '{} :: {:,d} 원\n'.format(name, price)
+
+    # send result as telegram message
+    method = "sendMessage"
+    chat_id = sender_id
+    api_url = f"https://api.telegram.org/bot{token}/{method}?chat_id={chat_id}&text={text}"
+    response = requests.get(api_url)
+    print(response)
